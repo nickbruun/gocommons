e8434423e7711a3f3e0d863c59df447fee1653e0
Nick Bruun <nick@bruun.co>
2015-04-07 23:46:58 +0200

    Slight documentation update for locking.

--- distributed/locking/doc.go
+++ distributed/locking/doc.go
@@ -1,2 +1,6 @@
 // Package locking provides distributed locking.
+//
+// Locking is provided by a LockProvider implementation, from which specific
+// locks are retrieved. These locks can subsequently be used to obtain mutually
+// exclusive locking.
 package locking
