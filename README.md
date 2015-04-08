cde3bd5e0dc763c61b037ba61dbf4fc2d38a6a7b
Nick Bruun <nick@bruun.co>
2015-04-08 02:12:12 +0200

    Update logging and README.

--- README.md
+++ README.md
@@ -3,3 +3,51 @@
 [![GoDoc](https://godoc.org/github.com/nickbruun/gocommons?status.svg)](https://godoc.org/github.com/nickbruun/gocommons) [![Build status](https://travis-ci.org/nickbruun/gocommons.svg?branch=master)](https://travis-ci.org/nickbruun/gocommons)
 
 Common libraries for the type of Go applications I toy with and build.
+
+
+## Contents
+
+The libraries are a mixture of foundation, utility and system libraries for various purposes.
+
+
+### Foundation
+
+**logging**
+
+A thin wrapper library around [Simon Eskildsen's](http://sirupsen.com/) excellent [logrus](https://github.com/Sirupsen/logrus) that provides caller annotations to make it easier to figure out where log messages are coming from:
+
+```Go
+package main
+
+import (
+	logrus "github.com/Sirupsen/logrus"
+	log "github.com/nickbruun/gocommons/logging"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func MyFunc() {
+	log.Debug("Debug time!")
+
+	wg.Add(1)
+	go func() {
+		log.Info("Go-routines are presented understandable as well")
+		wg.Done()
+	}()
+	wg.Wait()
+}
+
+func main() {
+	logrus.SetLevel(logrus.DebugLevel)
+
+	log.Info("Hola, mundo")
+	MyFunc()
+}
+```
+
+Results in:
+
+    INFO[0000] Hola, mundo                                       file=/Users/nick/Code/gocommonsdev/src/github.com/nickbruun/gocommons/play/main.go func=main.main line=25
+    DEBU[0000] Debug time!                                       file=/Users/nick/Code/gocommonsdev/src/github.com/nickbruun/gocommons/play/main.go func=main.MyFunc line=12
+    INFO[0000] Go-routines are presented understandable as well  file=/Users/nick/Code/gocommonsdev/src/github.com/nickbruun/gocommons/play/main.go func=main.func·001 line=16
--- logging/logging.go
+++ logging/logging.go
@@ -2,61 +2,92 @@
 package logging
 
 import (
-	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"runtime"
 )
 
-func identifyCaller(skip int) string {
-	pc, _, _, ok := runtime.Caller(skip)
+// Get a caller-identifying logger.
+func identified(logger *log.Logger, skip int) *log.Entry {
+	pc, file, line, ok := runtime.Caller(skip)
 	if !ok {
-		return "<unknown>"
+		return log.NewEntry(logger)
 	}
 
 	fn := runtime.FuncForPC(pc)
 	if fn == nil {
-		return "<unknown>"
+		return logger.WithFields(log.Fields{
+			"file": file,
+			"line": line,
+		})
 	}
 
-	return fn.Name()
+	return logger.WithFields(log.Fields{
+		"file": file,
+		"line": line,
+		"func": fn.Name(),
+	})
 }
 
 func Debug(args ...interface{}) {
-	log.Debug(fmt.Sprintf("[%s] %s", identifyCaller(2), fmt.Sprint(args...)))
+	identified(log.StandardLogger(), 2).Debug(args...)
 }
 
 func Debugf(format string, args ...interface{}) {
-	log.Debug(fmt.Sprintf("[%s] %s", identifyCaller(2), fmt.Sprintf(format, args...)))
+	identified(log.StandardLogger(), 2).Debugf(format, args...)
 }
 
 func Info(args ...interface{}) {
-	log.Info(fmt.Sprintf("[%s] %s", identifyCaller(2), fmt.Sprint(args...)))
+	identified(log.StandardLogger(), 2).Info(args...)
 }
 
 func Infof(format string, args ...interface{}) {
-	log.Info(fmt.Sprintf("[%s] %s", identifyCaller(2), fmt.Sprintf(format, args...)))
+	identified(log.StandardLogger(), 2).Infof(format, args...)
+}
+
+func Print(args ...interface{}) {
+	identified(log.StandardLogger(), 2).Print(args...)
+}
+
+func Printf(format string, args ...interface{}) {
+	identified(log.StandardLogger(), 2).Printf(format, args...)
 }
 
 func Warn(args ...interface{}) {
-	log.Warn(fmt.Sprintf("[%s] %s", identifyCaller(2), fmt.Sprint(args...)))
+	identified(log.StandardLogger(), 2).Warn(args...)
 }
 
 func Warnf(format string, args ...interface{}) {
-	log.Warn(fmt.Sprintf("[%s] %s", identifyCaller(2), fmt.Sprintf(format, args...)))
+	identified(log.StandardLogger(), 2).Warnf(format, args...)
+}
+
+func Warning(args ...interface{}) {
+	identified(log.StandardLogger(), 2).Warning(args...)
+}
+
+func Warningf(format string, args ...interface{}) {
+	identified(log.StandardLogger(), 2).Warningf(format, args...)
 }
 
 func Error(args ...interface{}) {
-	log.Error(fmt.Sprintf("[%s] %s", identifyCaller(2), fmt.Sprint(args...)))
+	identified(log.StandardLogger(), 2).Error(args...)
 }
 
 func Errorf(format string, args ...interface{}) {
-	log.Error(fmt.Sprintf("[%s] %s", identifyCaller(2), fmt.Sprintf(format, args...)))
+	identified(log.StandardLogger(), 2).Errorf(format, args...)
+}
+
+func Panic(args ...interface{}) {
+	identified(log.StandardLogger(), 2).Panic(args...)
+}
+
+func Panicf(format string, args ...interface{}) {
+	identified(log.StandardLogger(), 2).Panicf(format, args...)
 }
 
 func Fatal(args ...interface{}) {
-	log.Fatal(fmt.Sprintf("[%s] %s", identifyCaller(2), fmt.Sprint(args...)))
+	identified(log.StandardLogger(), 2).Fatal(args...)
 }
 
 func Fatalf(format string, args ...interface{}) {
-	log.Fatal(fmt.Sprintf("[%s] %s", identifyCaller(2), fmt.Sprintf(format, args...)))
+	identified(log.StandardLogger(), 2).Fatalf(format, args...)
 }
